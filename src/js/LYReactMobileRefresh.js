2c91be6806c11020217cb4b36c38a5d43822698b
LYninG <lynnx4869@qq.com>
2016-03-28 13:43:42 +0800

    change src

--- src/js/LYReactMobileRefresh.js
+++ src/js/LYReactMobileRefresh.js
@@ -1,22 +1,5 @@
 var React = require('react');
-
-var LYCubeGrid = React.createClass({
-	render: function() {
-		return (
-			<div className="sk-cube-grid">
-				<div className="sk-cube sk-cube1"></div>
-			 	<div className="sk-cube sk-cube2"></div>
-			  	<div className="sk-cube sk-cube3"></div>
-			  	<div className="sk-cube sk-cube4"></div>
-			  	<div className="sk-cube sk-cube5"></div>
-			  	<div className="sk-cube sk-cube6"></div>
-			  	<div className="sk-cube sk-cube7"></div>
-			  	<div className="sk-cube sk-cube8"></div>
-			  	<div className="sk-cube sk-cube9"></div>
-			</div>
-		);
-	}
-});
+var LYCubeGrid = require('./LYCubeGrid');
 
 var LYReactMobileRefresh = React.createClass({
 	startY: 0,
